b103bd8753809412de6bfb63b74d86085acb75a5
Iain <iain@humblyarrogant.io>
2025-01-25 11:47:43 +0100

    Change

---
+++ billabear/api/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from billabear.api.checkout_api import CheckoutApi
+from billabear.api.customers_api import CustomersApi
+from billabear.api.invoices_api import InvoicesApi
+from billabear.api.payment_details_api import PaymentDetailsApi
+from billabear.api.payments_api import PaymentsApi
+from billabear.api.prices_api import PricesApi
+from billabear.api.products_api import ProductsApi
+from billabear.api.receipt_api import ReceiptApi
+from billabear.api.refunds_api import RefundsApi
+from billabear.api.subscriptions_api import SubscriptionsApi
+from billabear.api.usage_api import UsageApi
